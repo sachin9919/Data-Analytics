c9ca81c72c56550243ca80f357ac8d91140a8a73
sam <sachinmishra0080@gmail.com>
2026-01-21 22:27:10 +0530

    SQL-practise

---
+++ SQLQuery7.sql
@@ -0,0 +1,6 @@
+
+select *  from [dbo].[Employees]
+order by Salary
+
+select * from dbo.Employees
+order by Department asc , Salary desc -- no need to write asc it will do by default but you to write dssc
